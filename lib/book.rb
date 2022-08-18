aeda0911040fafef8542c0c8f9784715492853ac
moses ngige <moses32ngigz@gmail.com>
2022-08-18 22:54:41 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
+class Book 
+    attr_accessor :title , :author , :page_count , :genre , :turn_page
+    def initialize(title)
+        @title = title
+      
+    end
+    def properties (author,page_count,genre,turn_page)
+      
+        @author = author
+        @page_count = page_count
+        @genre = genre   
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
